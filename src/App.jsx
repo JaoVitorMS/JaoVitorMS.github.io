180d7140a1c6bdca22bbee1a67c8672293c42c54
JaoVitorMS <joao.vitorms1509@gmail.com>
2025-09-05 20:08:56 -0300

    Divs tests for not blank page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css"
 
 function App() {
   return (
-      <>
+    <>
+    <div className="container">
       <div className="hello-container">
         <h1>Hello World!</h1>
       </div>
       <div className="app-container">
         <p>Mãe eu tenho um site!</p>
       </div>
+    </div>
     </>
   )
 }
