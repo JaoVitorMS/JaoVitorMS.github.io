8cb26e93353f9cfd854ecd57595b1b315276251e
João Vitor <64005648+JaoVitorMS@users.noreply.github.com>
2026-05-15 21:28:50 -0300

    Change initial site mode to 'sorting-teams'

--- app/site-mode.ts
+++ app/site-mode.ts
@@ -2,4 +2,4 @@ import { signal } from '@angular/core';
 
 export type SiteMode = 'game-list' | 'sorting-teams' | 'coming-soon';
 
-export const siteMode = signal<SiteMode>('game-list');
+export const siteMode = signal<SiteMode>('sorting-teams');
